a9fbed65194937d0d2975c9c3a787434c08077e7
andori8 <falcsmike7@gmail.com>
2019-01-09 20:03:19 -0500

    added cards per category

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import MotivationCard from './MotivationCard';
+import FunnyCard from './FunnyCard';
+import SportsCard from './SportsCard';
+import LoveCard from './LoveCard';
 
 class Home extends Component {
 
   render() {
     return (
-      <div></div>
+      <Container fluid>
+        <Row>
+          <Col col-6><MotivationCard /></Col>
+          <Col col-6><FunnyCard /></Col>
+        </Row>
+        <Row>
+          <Col><SportsCard /></Col>
+          <Col><LoveCard /></Col>
+        </Row>
+      </Container>
     )
   }
 }
