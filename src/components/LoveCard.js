ef2e6b6f54450c5a5613af1edbc4646343a43353
andori8 <falcsmike7@gmail.com>
2019-01-12 22:16:32 -0500

    edited love card info

--- src/components/LoveCard.js
+++ src/components/LoveCard.js
@@ -10,7 +10,7 @@ const Example = (props) => {
         <CardImg top width="100%" src={Love} alt="Card image cap" />
         <CardBody>
           <CardTitle>Love</CardTitle>
-          <CardText>Quotes that Will Make You Reconsider the Definition of Love</CardText>
+          <CardText>Quotes to Remind You of the Importance of Love</CardText>
           <Button href="/categories/4">Love Quotes</Button>
         </CardBody>
       </Card>
